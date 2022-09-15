bd1b1d0f13b5d744c4d68b6ce22924fa45f519eb
shubhssays <shubhssays@gmail.com>
2022-09-15 16:25:56 +0530

    added basic code to server

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
+(async () => {
+  app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, function () {
+    console.log(
+      `Server started at http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`
+    );
+  });
+})();
